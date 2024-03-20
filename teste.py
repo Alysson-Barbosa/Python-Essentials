5b2d9684d53e6b351f9d6e63d2fd023e46ac6785
Alysson Barbosa <alyssonrmbarbosa@gmail.com>
2024-03-19 23:59:12 -0300

    sendTextMessage.py

--- teste.py
+++ teste.py
@@ -8,17 +8,17 @@ auth_token = "3dfc061ff411f0e81fa3910548312e03"
 client = Client(account_sid, auth_token)
 
 
-# LÓGICA DE PROGRAMAÇÃO
+# PROGRAMMING LOGIC
 
-# 1 - Abrir os 6 arquivos em Excel
+# 1 - Open the six Excel files sent by your boss 
 lista_meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho']
 
-# Para cada arquivo: - 2 Verificar se algum valor na coluna vendas daquele arquivo é maior que 55.000
+# For each file: - 2 Check if any value under 'Vendas' column is higher than 55,000
 
 for mes in lista_meses:
     tabela_vendas = pd.read_excel(f'{mes}.xlsx')
 
-    # 3 Se for maior que 55.000 -> Envia um SMS com o nome do vendedor e as vendas
+    # 3 If there's a value higher than 55,000 -> Send a text message with the employee name and the value of sales
 
     if (tabela_vendas['Vendas'] > 55000).any(): # any() para consultar se algum valor é maior que 55, não a quantidadde de linhas na tabela
         vendedor = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendedor'].values[0]
@@ -30,5 +30,5 @@ for mes in lista_meses:
                 body=f'No mês {mes} alguém bateu a meta. Vendedor: {vendedor}, Vendas: {vendas}')
         print(message.sid)
 
-# Caso não seja maior que 55.000 não vou fazer nada.
+# If there's no value above 55,000 don't send any message.
 
